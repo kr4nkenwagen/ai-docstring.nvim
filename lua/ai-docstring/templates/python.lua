7a25252c088b29ef37874a5d64b81dcdc6f7aa54
Jon Jennemann <github@jonf.se>
2025-11-12 07:38:04 +0100

    Add python template

---
+++ lua/ai-docstring/templates/python.lua
@@ -0,0 +1,14 @@
+local t = {}
+t.docstring = [[ """{{Brief description}}
+
+    Args:
+        {{arg1 name}} ({{arg1 type}}): {{arg1 description}}
+        {{arg2 name}} ({{arg2 type}}): {{arg2 description}}
+
+    Returns:
+        {{return type}}: {{return description}}
+
+    Raises:
+        {{TypeError}}: {{Error description}}
+    """]]
+return t
--- lua/ai-docstring/utils/ai-wrapper.lua
+++ lua/ai-docstring/utils/ai-wrapper.lua
@@ -1,11 +1,12 @@
 local w = {}
 function w.query(question, language)
 	local config = require("ai-docstring").config
+	local docstring = require("ai-docstring.templates." .. language)
 	local cmd = {
 		"ollama",
 		"run",
 		config.ai.model,
-		'"' .. config.ai.system:gsub("$LANG", language) .. " " .. question .. '"',
+		'"' .. config.ai.system:gsub("$LANG", language):gsub("$TEMPLATE", docstring) .. " " .. question .. '"',
 	}
 	local window = require("ai-docstring.window")
 	local runner = require("ai-docstring.runner")
