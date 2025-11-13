00874449780ecf4341646a1d1501c1297ea3cd6e
Jon Jennemann <github@jonf.se>
2025-11-13 08:14:08 +0100

    Changed system to prompt and added $FUNC

--- lua/ai-docstring/config.lua
+++ lua/ai-docstring/config.lua
@@ -5,7 +5,7 @@ c.decline_key = "q"
 c.renew_key = "r"
 c.ai = {
 	model = "mistral:7b",
-	system = "Fill in the template with information from the $LANG function at the end. do not write code. Write only the template. with data. Nothing else: $TEMPLATE",
+	prompt = "Fill in the template with information from the $LANG function at the end. do not write code. Write only the template. with data. Nothing else: $TEMPLATE \n\n$FUNC",
 	serve = true,
 }
 return c
